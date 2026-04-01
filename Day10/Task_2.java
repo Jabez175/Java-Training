811b1cd53272fc5997728fe44ca92476a30527e6
Jabez175 <geoffreyjabez.175@gmail.com>
2026-04-01 08:23:35 +0530

    Day10

---
+++ Day10/Task_2.java
@@ -0,0 +1,24 @@
+/* 
+Design document editor where iser create different types of documents like textdoc, imagedoc, spredsheet 
+
+1.requirements 
+- each document 
+    open
+    save
+    close
+
+2.Different dicument types behaves differently
+- text -> shows text content 
+- image -> renders image
+spreadsheet -> calculates cells 
+
+Rules:
+
+user should not know internal logic
+document data must be protected
+System should allow adding new document type.
+
+rules1: abstracction
+rules2: encapsulation
+rules3: inheritance
+*/
